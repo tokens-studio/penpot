ee67f46408314d15d0190ef0b274d12d6604e464
Florian Schroedl <flo.schroedl@gmail.com>
2025-11-20 15:29:31 +0100

    :construction: Custom form

--- frontend/src/app/main/data/style_dictionary.cljs
+++ frontend/src/app/main/data/style_dictionary.cljs
@@ -587,7 +587,9 @@
                     (rx/sub! resolved-tokens-s (fn [resolved-tokens]
                                                  (swap! cache-atom assoc tokens resolved-tokens)
                                                  (reset! tokens-state resolved-tokens))))))))
-    @tokens-state))
+    (if (contains? cf/flags :tokenscript)
+      (ts/resolve-tokens tokens)
+      @tokens-state)))
 
 (defn use-resolved-tokens*
   "This hook will return the unresolved tokens as state until they are
